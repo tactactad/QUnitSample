33f9d99c2a37d8b53c03ce996be1475c2ef4893b
Takuro Shimoda <tac@reiare.net>
2011-10-26 12:00:07 +0900

    impl basic test case
    基本のテストの書き方を書いた。

--- logic.js
+++ logic.js
@@ -1,2 +1,9 @@
 var bool = true;
 var num = 1;
+var str = 'a';
+var obj = {};
+var obj2 = obj;
+
+function returnTrue() {
+    return true;
+}
--- test.js
+++ test.js
@@ -1,2 +1,43 @@
-module('QUnitSample', {
+test('a test', function () {
+    ok(true, "always fine");
 });
+test('primitive', function () {
+    expect(7);
+    ok(bool);
+    deepEqual(num, 1);
+    notDeepEqual(num, '1');
+    deepEqual(str, 'a');
+    deepEqual(obj, {});
+    strictEqual(obj, obj2);
+    notStrictEqual(obj, {});
+});
+test('raises', function () {
+    expect(1);
+    raises(function () {
+        throw new Error();
+    }, 'throw error');
+});
+test('function', function () {
+    expect(1);
+    ok(returnTrue());
+});
+asyncTest('async', function () {
+    expect(1);
+    setTimeout(function () {
+        ok(true);
+        start();
+    }, 10);
+});
+module('module1', {
+    setup: function () {
+        console.log('module1 setup...');
+    },
+    teardown: function () {
+        console.log('module1 teardonw...');
+    }
+});
+test('module1 dummmy', function () {
+    expect(0);
+    console.log('module1 dummy');
+});
+
